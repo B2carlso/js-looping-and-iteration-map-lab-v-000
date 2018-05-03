3de9bb110175d85ab9a99aaed4be3bf6e9c08169
Brad Carlson <b2carlso@yahoo.com>
2018-05-03 00:46:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function lowerCaseDrivers (drivers) {
   return drivers.map(function(drivers) {return drivers.toLowerCase()});
 }
+
+function nameToAttributes(drivers) {
+  return drivers.map(function(driver) {
+    names = driver.split(" ");
+    return {firstName: names[0], lastName: names[1]}
+  })
+}
